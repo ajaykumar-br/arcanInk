3bf626ed12406ed96225c58291e25c2fe40ca218
Ajay Kumar B R <ajraj86@gmail.com>
2025-01-18 18:56:59 -0500

    http-backend completed and tested with postman

---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+
+export interface CustomRequest extends Request {
+    userId?: string;
+}
+
+export function middleware(req: CustomRequest, res: Response, next: NextFunction) {
+    const token = req.headers["authorization"] ?? "";
+
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+
+    if(decoded) {
+        req.userId = decoded.userId;
+        next();
+    } else {
+        res.status(403).json({
+            msg: "unauthorized request"
+        });
+    }
+}
